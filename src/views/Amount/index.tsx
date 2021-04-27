5a32b545ef18dd84f7a7169e1bd9ad42172bec7b
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-04-27 18:42:09 +0100

    Styling changes to Amount view

--- src/views/Amount/index.tsx
+++ src/views/Amount/index.tsx
@@ -5,23 +5,21 @@ import { Input } from '../../components/Input';
 export function Amount() {
   return (
     <section>
-      <div className="header">
-        <h1 className="text-purple-dark text-lg">One-time Payout</h1>
-        <h2 className="text-purple-light text-sm">Send money internationally</h2>
+      <h1 className="text-purple-dark text-lg font-medium">One-time Payout</h1>
+      <h2 className="text-purple-light text-sm">Send money internationally</h2>
 
-        <form className="mt-8">
-          <Input label="You send" />
-          <br />
-          <Input label="Recipient gets" />
+      <form className="mt-8">
+        <Input label="You send" />
+        <br />
+        <Input label="Recipient gets" />
 
-          <div className="grid grid-cols-2 gap-4 mt-8">
-            <Button className="border border-purple-normal text-purple-normal">
-              Compare Rates
-            </Button>
-            <Button className="bg-purple-accent text-white">Continue</Button>
-          </div>
-        </form>
-      </div>
+        <div className="grid grid-cols-2 gap-4 mt-8">
+          <Button className="border border-purple-normal text-purple-normal">
+            Compare Rates
+          </Button>
+          <Button className="bg-purple-normal text-white">Continue</Button>
+        </div>
+      </form>
     </section>
   );
 }
