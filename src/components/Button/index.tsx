577ba9e6dadc88ba65bbd467ee4af2748f19f940
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-04-27 23:04:09 +0100

    Added handleClick prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 interface Props extends React.PropsWithChildren<unknown> {
   className: string;
+  handleClick?: () => void;
 }
 
-export function Button({ children, className }: Props) {
+export function Button({ children, className, handleClick = () => {} }: Props) {
   return (
-    <button className={'font-semibold rounded px-4 py-4 text-sm ' + className}>
+    <button
+      className={'font-semibold rounded px-4 py-4 text-sm ' + className}
+      onClick={handleClick}
+    >
       {children}
     </button>
   );
