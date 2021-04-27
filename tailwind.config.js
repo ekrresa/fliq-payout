cd7028851bfd07de36070298208ec375d11f81e8
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-04-27 08:37:00 +0100

    Added custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,30 @@
 module.exports = {
-  purge: [
-    "public/**/*.html",
-    "src/**/*.js",
-    "src/**/*.jsx",
-    "src/**/*.ts",
-    "src/**/*.tsx",
-  ],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "public/**/*.html",
+      "src/**/*.js",
+      "src/**/*.jsx",
+      "src/**/*.ts",
+      "src/**/*.tsx",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          dark: "#2C0C6A",
+          light: "#877E9E",
+          normal: "#4305EB",
+          accent: "#A98CF6",
+        },
+        edge: "#E8E8E8",
+        main: {
+          light: "#8B8F96",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
