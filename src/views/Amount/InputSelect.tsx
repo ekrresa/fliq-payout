fe0f47325c25432ff100a60e75cb18d59ceb19b5
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-04-28 22:35:49 +0100

    Added props to InputSelect to extract data

--- src/views/Amount/InputSelect.tsx
+++ src/views/Amount/InputSelect.tsx
@@ -1,20 +1,70 @@
-import { Select } from '../../components/Select';
+import { useEffect, useState } from 'react';
+import ReactSelect from 'react-select';
+import styled from 'styled-components';
+
+import { useCountries } from './utils/countryMap';
 
 interface Props {
   label: string;
+  name: string;
+  handleChange: (data: ChangeProps) => void;
+  value?: string;
+  readonly?: boolean;
 }
 
-export function InputSelect({ label }: Props) {
+export interface ChangeProps {
+  amount?: string;
+  currency?: string;
+  name: string;
+}
+
+export function InputSelect({ label, name, handleChange, readonly, value }: Props) {
+  const countries = useCountries();
+  const [data, setData] = useState<ChangeProps>({ name });
+
+  useEffect(() => {
+    handleChange(data);
+  }, [handleChange, data]);
+
   return (
-    <div className="flex focus-within:outline-solid px-4 py-2 border border-edge justify-between rounded">
-      <div className="flex-1 mr-2">
-        <label className="block text-main-light text-xs" htmlFor="amount">
+    <div className="flex focus-within:outline-solid  border border-edge justify-between rounded">
+      <div className="flex-1 mr-2 px-4 py-4">
+        <label className="block text-main-light text-sm" htmlFor={name}>
           {label}
         </label>
-        <input className="block w-full focus:outline-none" id="amount" />
+        <input
+          className="block w-full focus:outline-none disabled:bg-white"
+          id={name}
+          onChange={e => {
+            setData({ ...data, amount: e.target.value });
+          }}
+          value={value ?? ''}
+          readOnly={readonly}
+        />
       </div>
 
-      <Select />
+      <div className="bg-main-spark2 flex flex-col flex-1 max-w-9 justify-center px-4">
+        <StyledSelect
+          options={countries}
+          isClearable={false}
+          isSearchable
+          onChange={(item: any) => {
+            setData({ ...data, currency: item.value });
+          }}
+          name={name}
+        />
+      </div>
     </div>
   );
 }
+
+const StyledSelect = styled(ReactSelect)`
+  .css-yk16xz-control {
+    border: none;
+    background: inherit;
+  }
+
+  .css-1okebmr-indicatorSeparator {
+    display: none;
+  }
+`;
--- src/views/Amount/index.tsx
+++ src/views/Amount/index.tsx
@@ -1,8 +1,11 @@
+import { useCallback } from 'react';
 import { useHistory } from 'react-router';
 
 import { Button } from '../../components/Button';
-import { InputSelect } from './InputSelect';
+import { ChangeProps, InputSelect } from './InputSelect';
 import { TransferDetails } from './TransferDetails';
+
+const TRANSFER_FEE_IN_DOLLARS = 3.56;
 
 export default function Amount() {
   const history = useHistory();
@@ -16,16 +19,26 @@ export default function Amount() {
     e.preventDefault();
   };
 
+  const handleInputSelect = useCallback(({ ...data }: ChangeProps) => {
+    console.log({ data });
+  }, []);
+
   return (
     <section>
       <h1 className="text-purple-dark text-lg font-medium">One-time Payout</h1>
       <h2 className="text-purple-light text-sm">Send money internationally</h2>
 
       <form className="mt-8" onSubmit={handleSubmit}>
-        <InputSelect label="You send" />
+        <InputSelect label="You send" name="from_currency" handleChange={handleInputSelect} />
         {/* <br /> */}
-        <TransferDetails />
-        <InputSelect label="Recipient gets" />
+        <TransferDetails transferFee={TRANSFER_FEE_IN_DOLLARS} />
+        <InputSelect
+          label="Recipient gets"
+          name="to_currency"
+          handleChange={handleInputSelect}
+          value="12345"
+          readonly
+        />
 
         <div className="grid grid-cols-2 gap-4 mt-8">
           <Button className="border border-purple-normal text-purple-normal">
