3899a72c887cbe6a42b383d9244d17ae32939b6f
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-04-28 18:17:26 +0100

    Initialize react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './tailwind.output.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
